fe44160af83830d60a7553a7641fb808b9ea843c
Julian Gruber <julian@juliangruber.com>
2013-08-12 11:22:28 -0400

    compress cached bundles by default

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 var browserify = require('browserify');
 var Cache = require('waiting-cache');
 var through = require('through');
+var uglify = require('uglify-js');
 
-module.exports = function (index, fn) {
+module.exports = function (index, opts, fn) {
+  if ('function' == typeof opts) {
+    fn = opts;
+    opts = {};
+  }
+  
+  opts = opts || {};
   var cache = new Cache();
-
-  var b = browserify(index);
-  if (fn) fn(b);
-  b.bundle(function (err, bundle) {
-    cache.set('bundle', bundle);
-  });
+  
+  function getBundle () {
+    var b = browserify(index);
+    if (fn) fn(b);
+    return b;
+  }
 
   return function (cached) {
     var tr = through();
-    var b = browserify(index);
-    if (fn) fn(b);
 
     if (cached) {
       cache.get('bundle', function (bundle) {
         if (bundle) return tr.end(bundle);
+        
         cache.caching('bundle');
-        b.bundle(function (err, bundle) {
+        getBundle().bundle(function (err, bundle) {
+          if (err) {
+            cache.set('bundle', false);
+            tr.emit('error', err);
+            return;
+          }
+          
+          if (opts.compress !== false) bundle = compress(bundle);
           cache.set('bundle', bundle);
-          if (err) tr.emit('error', err);
-          else tr.end(bundle);
+          tr.end(bundle);
         });
       });
     } else {
-      b.bundle({
+      getBundle().bundle({
         debug: true,
         insertGlobals: true
       }).pipe(tr);
@@ -37,3 +49,15 @@ module.exports = function (index, fn) {
   }
 };
 
+function compress (src) {
+  var ast = uglify.parse(src, {});
+  ast.figure_out_scope();
+  var compressor = uglify.Compressor({
+    warnings: false
+  });
+  var compressed = ast.transform(compressor);
+  compressed.figure_out_scope();
+  compressed.compute_char_frequency();
+  compressed.mangle_names();
+  return compressed.print_to_string();
+}
